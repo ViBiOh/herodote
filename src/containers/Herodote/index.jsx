a5d08534548713d595b16962e4e99e947fc3bc86
Vincent Boutour <bob@vibioh.fr>
2020-07-06 21:03:40 +0200

    feat: Adding URL store of state for better sharing

--- src/containers/Herodote/index.jsx
+++ src/containers/Herodote/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { search } from "services/Algolia";
+import { search as algoliaSearch } from "services/Algolia";
+import { search as urlSearch, push as urlPush } from "helpers/URL";
 import CommitsList from "components/CommitsList";
 import "./index.css";
 
-const filterRegex = /([^\s]+:[^\s]+)/gim;
+const filterRegex = /([^\s]+):([^\s]+)/gim;
 
 /**
  * Herodote Component.
@@ -19,7 +20,7 @@ export default class Herodote extends Component {
     this._isMounted = false;
 
     this.state = {
-      q: "",
+      query: "",
       results: [],
       nextPage: 0,
       pagesCount: 0,
@@ -31,7 +32,9 @@ export default class Herodote extends Component {
    */
   componentDidMount() {
     this._isMounted = true;
-    this.filterBy("");
+
+    const { query } = urlSearch();
+    this.filterBy(query);
   }
 
   /**
@@ -42,57 +45,83 @@ export default class Herodote extends Component {
   }
 
   /**
+   * React lifecycle.
+   */
+  componentDidCatch(error, info) {
+    global.console.error(error, info.componentStack);
+  }
+
+  /**
    * Filter content with given query
    * @param  {String} query Query string
    */
-  filterBy = async (q, page = 0) => {
-    const filters = [];
-    q.replace(filterRegex, (all, filter) => {
-      filters.push(filter);
+  filterBy = async (query = "", page) => {
+    const filtersValues = {};
+    query.replace(filterRegex, (all, key, value) => {
+      filtersValues[key] = (filtersValues[key] || []).concat(value);
     });
 
-    const query = q.replace(filterRegex, "");
+    const filters = Object.entries(filtersValues).map(([key, values]) =>
+      values.map((value) => `${key}:${value}`).join(" OR ")
+    );
 
-    try {
-      const output = await search(query, page, {
-        filters: filters.join(" AND "),
-      });
+    await this.fetchCommits(query.replace(filterRegex, ""), filters, page);
 
-      if (this._isMounted && output) {
-        let list = output.hits;
-        if (page > 0) {
-          list = [...this.state.results, ...list];
-        }
+    this.setState({ query });
+    urlPush({ query });
+  };
 
-        this.setState({
-          q,
-          results: list,
-          nextPage: page + 1,
-          pagesCount: output.nbPages,
-        });
+  /**
+   * Fetch commits from algolia backend
+   * @param  {String} query   Search query
+   * @param  {Array}  filters List of filters
+   * @param  {Number} page    Page, 0 based
+   */
+  fetchCommits = async (query, filters, page = 0) => {
+    const output = await algoliaSearch(query, {
+      filters: filters.length ? `(${filters.join(") AND (")})` : "",
+      page,
+    });
+
+    if (this._isMounted && output) {
+      let results = output.hits;
+      if (page) {
+        results = this.state.results.concat(results);
       }
-    } catch (e) {
-      global.console.error(`unable to filter by '${q}': ${e}`);
+
+      this.setState({
+        results,
+        nextPage: output.page + 1,
+        pagesCount: output.nbPages,
+      });
     }
   };
 
   /**
-   * Debounced tigger when user change input
+   * Debounced trigger when user change input
    * @param  {Object} e Input
    */
   onSearchChange = (e) => {
     clearTimeout(this.timeout);
 
+    const query = e.target.value;
+    this.setState({ query });
+
     ((text) => {
       this.timeout = setTimeout(() => this.filterBy(text), 300);
-    })(e.target.value);
+    })(query);
   };
 
   /**
    * Fetch next page
    */
   onMoreClick = () => {
-    this.filterBy(this.state.q, this.state.nextPage);
+    console.log(
+      "this.state.nextPage",
+      typeof this.state.nextPage,
+      this.state.nextPage
+    );
+    this.filterBy(this.state.query, this.state.nextPage);
   };
 
   /**
@@ -108,6 +137,7 @@ export default class Herodote extends Component {
           placeholder="Filter commit..."
           className="search padding"
           onChange={this.onSearchChange}
+          value={this.state.query}
         />
 
         <hr />
