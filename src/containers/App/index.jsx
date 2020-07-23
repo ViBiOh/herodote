2bb44fb7e7ef006af61c0c6fbfb8bfa766611a9d
Vincent Boutour <bob@vibioh.fr>
2020-07-23 08:50:30 +0200

    refactor: Moving URL read and query to App instead of Filters

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import getConfig from 'services/Config';
 import { init as initAlgolia } from 'services/Algolia';
+import { search as urlSearch } from 'helpers/URL';
 import Header from 'components/Header';
 import Filters from 'containers/Filters';
 import Herodote from 'containers/Herodote';
@@ -22,13 +23,22 @@ export default function App() {
     })();
   }, []);
 
+  useEffect(() => {
+    const { query: searchQuery } = urlSearch();
+    if (searchQuery) {
+      setQuery(searchQuery);
+    }
+  }, []);
+
   return (
     <div className="content">
       <Header />
 
       <div className="padding full">
         {!config && <Throbber label="Loading configuration..." />}
-        {config && <Filters onChange={setQuery} filters={filters} />}
+        {config && (
+          <Filters query={query} onChange={setQuery} filters={filters} />
+        )}
         {config && <Herodote query={query} setFilters={setFilters} />}
       </div>
     </div>
