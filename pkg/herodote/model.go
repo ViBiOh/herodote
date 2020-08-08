5d7ea19f843f5e0aa682a200eb76c17741d13869
Vincent Boutour <bob@vibioh.fr>
2020-08-08 22:25:25 +0200

    feat(db): Updating herodote.sh script and improving insert speed

--- pkg/herodote/model.go
+++ pkg/herodote/model.go
@@ -5,25 +5,17 @@ import (
 	"strings"
 )
 
-// Repository describes a repository
-type Repository struct {
-	ID     uint64
-	Name   string
-	Remote string
-}
-
 // Commit describes a commit on a repository
 type Commit struct {
-	Hash      string
-	Type      string
-	Component string
-	Revert    bool
-	Breaking  bool
-	Content   string
-	Date      uint64
-
-	Remote     string
-	Repository string
+	Hash       string `json:"hash"`
+	Type       string `json:"type"`
+	Component  string `json:"component"`
+	Revert     bool   `json:"revert"`
+	Breaking   bool   `json:"breaking"`
+	Content    string `json:"content"`
+	Date       uint64 `json:"date"`
+	Remote     string `json:"remote"`
+	Repository string `json:"repository"`
 }
 
 // Sanitize cleans value of a commit
@@ -31,22 +23,12 @@ func (c Commit) Sanitize() Commit {
 	c.Hash = cleanString(c.Hash)
 	c.Type = cleanString(c.Type)
 	c.Component = cleanString(c.Component)
-	c.Remote = cleanString(c.Remote)
-	c.Repository = cleanString(c.Repository)
 
 	return c
 }
 
 // Check verifies that Commit is valid
 func (c Commit) Check() error {
-	if len(c.Remote) == 0 {
-		return fmt.Errorf("repository's remote is required (e.g. `github.com`)")
-	}
-
-	if len(c.Remote) == 0 {
-		return fmt.Errorf("repository's name is required (e.g. `vibioh/herodote`)")
-	}
-
 	if len(c.Hash) == 0 {
 		return fmt.Errorf("commit's hash is required (e.g. `1ab2c3f4d`)")
 	}
@@ -56,7 +38,19 @@ func (c Commit) Check() error {
 	}
 
 	if len(c.Content) == 0 {
-		return fmt.Errorf("commit's description is required (e.g. `Add README.md`)")
+		return fmt.Errorf("commit's content is required (e.g. `Add README.md`)")
+	}
+
+	if c.Date == 0 {
+		return fmt.Errorf("commit's date is required (e.g. `1596913344`)")
+	}
+
+	if len(c.Remote) == 0 {
+		return fmt.Errorf("repository's remote is required (e.g. `github.com`)")
+	}
+
+	if len(c.Repository) == 0 {
+		return fmt.Errorf("repository's name is required (e.g. `vibioh/herodote`)")
 	}
 
 	return nil
