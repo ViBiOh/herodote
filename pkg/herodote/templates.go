341f166e404d733f7f70652e85c696ac1f28032e
Vincent Boutour <bob@vibioh.fr>
2021-01-16 21:05:55 +0100

    feat(a11y): Changing colors for a11y complianc

--- pkg/herodote/templates.go
+++ pkg/herodote/templates.go
@@ -20,22 +20,21 @@ const (
 
 var (
 	repositoriesColors = []string{
-		"#006E6D",
-		"#2A4B7C",
-		"#3F69AA",
-		"#77212E",
-		"#577284",
-		"#6C4F3D",
-		"#797B3A",
-		"#935529",
-		"#BD3D3A",
-		"#9B1B30",
-		"#E08119",
-		"#6B5B95",
-		"#F96714",
-		"#485167",
-		"#2E4A62",
-		"#264E36",
+		"aqua",
+		"aquamarine",
+		"cadetblue",
+		"coral",
+		"cornflowerblue",
+		"darkseagreen",
+		"deepskyblue",
+		"greenyellow",
+		"lightgreen",
+		"limegreen",
+		"orange",
+		"peachpuff",
+		"salmon",
+		"springgreen",
+		"tomato",
 	}
 	colorsCount = 0
 	colors      = sync.Map{}
