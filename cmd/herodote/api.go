c346ca5c3ebe2d7687fa11c776ef4d6bcff86b14
Vincent Boutour <bob@vibioh.fr>
2021-06-04 11:51:43 +0200

    feat(pprof): Removing registered pprof listener
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/herodote/api.go
+++ cmd/herodote/api.go
@@ -3,8 +3,6 @@ package main
 import (
 	"embed"
 	"flag"
-	"net/http"
-	_ "net/http/pprof"
 	"os"
 
 	"github.com/ViBiOh/herodote/pkg/herodote"
@@ -31,7 +29,6 @@ func main() {
 
 	appServerConfig := server.Flags(fs, "")
 	promServerConfig := server.Flags(fs, "prometheus", flags.NewOverride("Port", 9090), flags.NewOverride("IdleTimeout", "10s"), flags.NewOverride("ShutdownTimeout", "5s"))
-	pprofServerConfig := server.Flags(fs, "pprof", flags.NewOverride("Port", 9999))
 	healthConfig := health.Flags(fs, "")
 
 	alcotestConfig := alcotest.Flags(fs, "")
@@ -52,7 +49,6 @@ func main() {
 
 	appServer := server.New(appServerConfig)
 	promServer := server.New(promServerConfig)
-	pprofServer := server.New(pprofServerConfig)
 	prometheusApp := prometheus.New(prometheusConfig)
 
 	herodoteDb, err := db.New(dbConfig)
@@ -74,7 +70,6 @@ func main() {
 
 	rendererHandler := rendererApp.Handler(herodoteApp.TemplateFunc)
 
-	go pprofServer.Start("pprof", healthApp.End(), http.DefaultServeMux)
 	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
 	go appServer.Start("http", healthApp.End(), httputils.Handler(rendererHandler, healthApp, recoverer.Middleware, prometheusApp.Middleware, owasp.New(owaspConfig).Middleware, cors.New(corsConfig).Middleware))
 
